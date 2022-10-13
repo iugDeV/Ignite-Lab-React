42e5e598df284a0a57d23e316c92a11a8cc43114
Guilherme <aguilar.ggc@gmail.com>
2022-10-12 22:12:38 -0300

    Criação do primeiro component, e configuração do mesmo no storybook.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,6 @@ import './styles/global.css'
 
 export function App() {
   return (
-    <h1>Hello Word</h1>
+    <h1 className='text-purple-500'>Hello Word</h1>
     )
 }
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,44 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    '.src/**/*.tsx'
+    './src/**/*.tsx'
   ],
   theme: {
-    extend: {},
+    fontSize: {
+      xs: 14,
+      sm: 16,
+      md: 18,
+      lg: 20,
+      xl: 24,
+      "2xl": 32,
+    },
+    colors: {
+      black: "#aaa",
+      white: "#fff",
+
+      //GRAY
+      "gray-900": "#121214",
+      "gray-800": "#202024",
+      "gray-400": "#7c7c8a",
+      "gray-200": "#c4c4cc",
+      "gray-100": "#e1e1e6",
+
+      //CYAN
+      "cyan-500": "#81d8f7",
+
+      //GREEN
+      "green-900": "#016a3d",
+      "green-500": "#04d361",
+      "green-200": "#3fff95",
+
+      //PURPLE
+      "purple-500": "#633bbc",
+    },
+    extend: {
+      fontFamily: {
+        sans: "Roboto, sans-serif",
+      },
+    },
   },
   plugins: [],
-}
+};
